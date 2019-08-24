8cfaa13b0e07002ee169ca98ce1c06323bcf2855
Francesco Cabras <f.cabras@radixgroup.org>
2019-08-25 01:00:17 +0200

    added reactstrap example

--- examples/reactstrap-example/src/App.js
+++ examples/reactstrap-example/src/App.js
@@ -21,97 +21,97 @@ const model = {
 
 const validators = [];
 
-// const BasicLayout = (props) => {
-//   return (
-//     <div>
+const BasicLayout = (props) => {
+  return (
+    <div>
 
-//         {/* <Form> */}
-//           {props.model.groups.map((input, index)=> {
+        {/* <Form> */}
+          {props.model.groups.map((input, index)=> {
           
-//           const Component = props.components[input.type];
+          const Component = props.components[input.type];
 
-//           return (
-//             <FormGroup row key={`${index}`}>
-//               <Label style={{textAlign: 'left'}} sm={2} for={`${input.name}`}>{input.label}</Label>
-//               <Col sm={10}>
-//                 {props.components[input.type] !== undefined ? (
-//                     <Component model={input} store={props.store} />
-//                 ):(
-//                   <Input type={`${input.type}`} name={`${input.name}`} id={`${input.name}`}></Input>
-//                 )}
-//               </Col>
-//             </FormGroup>
+          return (
+            <FormGroup row key={`${index}`}>
+              <Label style={{textAlign: 'left'}} sm={2} for={`${input.name}`}>{input.label}</Label>
+              <Col sm={10}>
+                {props.components[input.type] !== undefined ? (
+                    <Component model={input} store={props.store} />
+                ):(
+                  <Input type={`${input.type}`} name={`${input.name}`} id={`${input.name}`}></Input>
+                )}
+              </Col>
+            </FormGroup>
             
-//           )})}
+          )})}
 
-//         {/* </Form> */}
-//     </div>
-//   )
-// }
+        {/* </Form> */}
+    </div>
+  )
+}
 
-// const ReactStrapForm = (props) => {
-//   return (
-//     <React.Fragment>
+const ReactStrapForm = (props) => {
+  return (
+    <React.Fragment>
 
-//       <span style={{color: 'darkgray', marginTop: 40, marginBottom: 40, display: 'block'}}>RXForm Reactstrap</span>
+      <span style={{color: 'darkgray', marginTop: 40, marginBottom: 40, display: 'block'}}>RXForm Reactstrap</span>
 
-//       <div style={{backgroundColor: "#eeeeee", padding:10}}>
-//         <Form>
-//           {props.children}
-//         </Form>
+      <div style={{backgroundColor: "#eeeeee", padding:10}}>
+        <Form>
+          {props.children}
+        </Form>
 
-//       </div>
+      </div>
 
-//     </React.Fragment>
-//   )
-// }
+    </React.Fragment>
+  )
+}
 
-// const BasicTextComponent = (props) => {
+const BasicTextComponent = (props) => {
 
-//   const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-//   const ref = useRef();
-//   const focused = useFocus(ref, props.model.name, props.store, props.model.validators ? props.model.validators : []);
+  const ref = useRef();
+  const focused = useFocus(ref, props.model.name, props.store, props.model.validators ? props.model.validators : []);
 
-//   // const [sub, setSub] = useState(null);
+  // const [sub, setSub] = useState(null);
 
-//   useEffect(()=>{
+  useEffect(()=>{
 
-//       // console.log(`in useEffect with store `, props.store);
-//       const unsubscribe = props.store.subscribe(()=>{
+      // console.log(`in useEffect with store `, props.store);
+      const unsubscribe = props.store.subscribe(()=>{
   
-//         const state = props.store.getState();
-//         console.log(`receive event effect`, state);
-//         setValue(state.values[props.model.name])
-//       });
-//       // console.log('after sub', unsubscribe);
+        const state = props.store.getState();
+        console.log(`receive event effect`, state);
+        setValue(state.values[props.model.name])
+      });
+      // console.log('after sub', unsubscribe);
 
-//     return ()=>{
-//       unsubscribe();
-//       console.log(`after unsubscribe`);
-//     } 
-//   }, [])
+    return ()=>{
+      unsubscribe();
+      console.log(`after unsubscribe`);
+    } 
+  }, [])
 
-//   const componentSetValue = (value) => {
-//     console.log(`in component set value with ${value}`)
-//     setValue(value);
-//     props.store.dispatch(setValueAction(props.model.name, value));
-//   } 
+  const componentSetValue = (value) => {
+    console.log(`in component set value with ${value}`)
+    setValue(value);
+    props.store.dispatch(setValueAction(props.model.name, value));
+  } 
 
-//   console.log(`in render2 for `, props.model.name);
-//   return (
-//     <React.Fragment>
-//         <Input innerRef={ref} type={`${props.model.type}`} name={`${props.model.name}`} id={`${props.model.name}`} value={value} onChange={(e) => componentSetValue(e.target.value)}></Input>
-//     </React.Fragment>
-//   )
-// }
+  console.log(`in render2 for `, props.model.name);
+  return (
+    <React.Fragment>
+        <Input innerRef={ref} type={`${props.model.type}`} name={`${props.model.name}`} id={`${props.model.name}`} value={value} onChange={(e) => componentSetValue(e.target.value)}></Input>
+    </React.Fragment>
+  )
+}
 
 const layouts = {
-  // default: BasicLayout
+  default: BasicLayout
 }
 
 const components = {
-  // text: BasicTextComponent,
+  text: BasicTextComponent,
 }
 
 export const  App = () => { 
@@ -140,24 +140,24 @@ export const  App = () => {
   return (
     <Container className="App">
       <RXForm
-        // formComponent={ReactStrapForm}
+        formComponent={ReactStrapForm}
         ref={form}
-        // layouts={layouts} 
-        // components={components} 
+        layouts={layouts} 
+        components={components} 
         model={model}
         data={{
           name: "Francesco",
           surname: "Cabras",
           email: "francesco.cabras@gmail.com"
         }} 
-        // validators={validators}
-        // onValidation={(errors)=>{
-        //   console.log(`in onValidation: `, JSON.stringify(errors));
-        //   setErrors(errors);
-        // }}
-        // onValuesChange={(values)=>{
-        //   console.log('in onValuesChange: ', values);
-        // }}
+        validators={validators}
+        onValidation={(errors)=>{
+          console.log(`in onValidation: `, JSON.stringify(errors));
+          setErrors(errors);
+        }}
+        onValuesChange={(values)=>{
+          console.log('in onValuesChange: ', values);
+        }}
       />
     </Container>
   );
